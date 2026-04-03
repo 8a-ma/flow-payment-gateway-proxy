0d496b543ab6309578f0c2c7a4feb36fa536e955
8a-ma <e0as0k98x001b@gmail.com>
2026-04-03 18:35:37 -0300

    feat: create payout order endpoint

--- src/main.js
+++ src/main.js
@@ -1,15 +1,83 @@
+// src/main.js
+require('dotenv').config();
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const hmac = require('node:crypto');
 
-const http = require('http')
+const app = express();
 
-const hostname = '127.0.0.1'
-const port = process.env.PORT || 3000;  
+// Middlewares de seguridad y logs
+app.use(helmet());
+app.use(morgan('combined'));
+app.use(express.json());
 
-const server = http.createServer( (req, res ) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text-plain')
-    res.end('Hello world!\n')
+const PORT = process.env.PORT || 8080; // Prioriza la variable de entorno
+const APP_NAME = process.env.APP_NAME || 'Flow Proxy';
+const FLOW_SECRET_KEY = process.env.FLOW_SECRET_KEY;
+const FLOW_API_KEY = process.env.FLOW_API_KEY;
+const FLOW_API_URL = process.env.FLOW_API_URL;
+
+app.get('/v1/status', (req, res) => {
+    res.status(204).end()
 })
 
-server.listen( port, hostname, () => {
-    console.log(`Server runnint at https://${hostname}:${port}/`)
-})
+
+app.post('/v1/payment/create', async (req, res) => {
+
+    try {
+        const paymentPayload = {
+            apiKey: FLOW_API_KEY,
+            ...req.body
+        };
+
+        const keys = Object.keys(paymentPayload);
+        keys.sort();
+
+        let toSign = "";
+
+        for (const key of keys) {
+            toSign += key + paymentPayload[key];
+        }
+
+        const signature = hmac
+            .createHmac("sha256", FLOW_SECRET_KEY)
+            .update(toSign)
+            .digest("hex");
+        
+        paymentPayload["s"] = signature;
+
+        const body = new URLSearchParams(paymentPayload).toString();
+
+        const response = await fetch(`${FLOW_API_URL}/payment/create`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: body
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            return res.status(response.status).json({
+                error: "Error from the Flow API",
+                detail: data
+            });
+        }
+
+        res.json(data);
+    } catch (error) {
+        console.log("Error on Flow Payment Create:", error);
+
+        res.status(500).json({
+            error: "Internal Server Error",
+            message: error.message
+        })
+    }
+
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+});
